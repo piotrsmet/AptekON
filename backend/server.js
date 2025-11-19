3602d8d53459ff23bd5495de5e1a2dd9e51f818f
smecik48 <piotrsmet@interia.pl>
2025-11-19 18:00:39 +0100

    database and first endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,46 @@
 import express from "express";
 import cors from "cors";
+import sqlite3 from "sqlite3"
+import fs from "fs"
+import {open} from "sqlite"
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// przykładowa trasa
+let db;
+
+async function initDb() {
+  db = await open({
+    filename: 'moja_baza.db',
+    driver: sqlite3.Database
+  });
+  console.log("Połączono z bazą SQLite.");
+}
+
+
+app.get("/apteki", async (req, res) =>{
+  try {
+    const apteki = await db.all("SELECT * FROM apteki");
+    res.json(apteki);
+  }  catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Błąd pobierania danych"});
+  }
+});
+
+app.get("/apteki/id", async (req, res) =>{
+  try {
+    const apteki = await db.all("SELECT id FROM apteki");
+    res.json(apteki);
+  }  catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Błąd pobierania danych"});
+  }
+});
+
+
 app.get("/api/matches", (req, res) => {
   res.json([
     { id: 1, teamA: "Real Madrid", teamB: "Barcelona", oddsA: 2.1, oddsB: 3.2 },
@@ -14,4 +49,8 @@ app.get("/api/matches", (req, res) => {
 });
 
 const PORT = 5000;
-app.listen(PORT, () => console.log(`Server działa na porcie ${PORT}`));
+initDb().then(() => {
+  app.listen(PORT, () => console.log(`Server działa na porcie ${PORT}`));
+}).catch(err => {
+  console.error("Błąd przy inicjalizacji bazy:", err);
+});
