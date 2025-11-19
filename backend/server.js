c8d5c10bbd2fa7ddc68e3526dbfe13a6f969024a
ms-matthew <mateusz.stachowicz1@wp.pl>
2025-11-19 18:38:58 +0100

    fixes

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ async function initDb() {
 const __dirname = path.resolve(); 
 app.use(express.static(path.join(__dirname, "../frontend", "dist")));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
 }); 
 
